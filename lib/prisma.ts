5f13e6c09e07e119b2abd2a4aaf08858e4c5cb90
amarquezsan <amarquezsan@gmail.com>
2025-08-29 10:33:50 -0400

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,10 @@
 
-import { PrismaClient } from '@prisma/client'
+import { prisma } from '@/lib/prisma'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma = globalForPrisma.prisma ?? new prisma()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
