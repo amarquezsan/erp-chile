3609683b24e2d7abc90345e4656052dc936d8122
amarquezsan <amarquezsan@gmail.com>
2025-08-29 19:40:55 -0400

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,7 +3,7 @@ import { NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import bcrypt from 'bcryptjs'
-import { prisma } from '@/lib/prisma'
+import { findUser, validatePassword } from '@/lib/mock-auth'
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
